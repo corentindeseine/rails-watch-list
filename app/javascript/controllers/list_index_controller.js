4a32331ceb336b853f1c18a9359641c501788cec
Corentin Deseine <deseinecorentin@gmail.com>
2022-08-12 14:05:03 +0200

    push

--- app/javascript/controllers/list_index_controller.js
+++ app/javascript/controllers/list_index_controller.js
@@ -8,9 +8,12 @@ export default class extends Controller {
   zoomCardIndex  (event) {
     gsap.timeline();
     gsap.to(event.currentTarget, {duration: 0.5, transformOrigin: "50% 50%", scale: 1.1 })
+    event.currentTarget.style.border = '2px solid #10ea96'
   }
 
   dezoomCardIndex (event) {
     gsap.to(event.currentTarget, {duration: 0.5, transformOrigin: "50% 50%", scale: 1 })
+    event.currentTarget.style.border = '0px solid #10ea96'
+
   }
 }
